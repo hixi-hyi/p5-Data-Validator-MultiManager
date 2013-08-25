978b1243f2ffd93acdc2edc75691d7bcaf71b222
Hiroyoshi Houchi <git@hixi-hyi.com>
2013-08-26 02:00:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Data::Validator::MultiManager - It's new $module
+
+# SYNOPSIS
+
+    use Data::Validator::MultiManager;
+
+# DESCRIPTION
+
+Data::Validator::MultiManager is ...
+
+# LICENSE
+
+Copyright (C) Hiroyoshi Houchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hiroyoshi Houchi <git@hixi-hyi.com>
---
+++ lib/Data/Validator/MultiManager.pm
@@ -0,0 +1,39 @@
+package Data::Validator::MultiManager;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Data::Validator::MultiManager - It's new $module
+
+=head1 SYNOPSIS
+
+    use Data::Validator::MultiManager;
+
+=head1 DESCRIPTION
+
+Data::Validator::MultiManager is ...
+
+=head1 LICENSE
+
+Copyright (C) Hiroyoshi Houchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hiroyoshi Houchi E<lt>git@hixi-hyi.comE<gt>
+
+=cut
+
